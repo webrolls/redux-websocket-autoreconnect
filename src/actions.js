bfcbe4189e58876d49f5a8da9349ea35d4d03bd2
tridev87 <it.tridev@gmail.com>
2019-01-11 09:49:37 -0800

    Changed the action file for modifying respone type

--- src/actions.js
+++ src/actions.js
@@ -30,13 +30,62 @@ export const closed = (event: Event): Action => ({
   }
 });
 
-export const message = (event: MessageEvent): Action => ({
-  type: WEBSOCKET_MESSAGE,
-  payload: {
-    timestamp: new Date(),
-    data: event.data,
-    event
+export const message = (event: MessageEvent): Action => {
+  // return {
+  //   type: WEBSOCKET_MESSAGE,
+  //   payload: {
+  //     timestamp: new Date(),
+  //     data: event.data,
+  //     event
+  //   }
+  // }
+  const data = JSON.parse(event.data);
+  const responseType = getResponseType(data);
+  console.log('Web Socket Response Type',responseType);
+  return {
+    type: responseType,
+    payload: {
+      timestamp: new Date(),
+      data: responseType === WEBSOCKET_MESSAGE ? event.data : data ,
+      event
+    },
   }
-});
+};
+
+
+const getResponseType = (data)=>{
+  let responseType = WEBSOCKET_MESSAGE;
+  const {command,action} = data;
+  if(command === 'qna'){
+    if(action === 'fetchQAndA'){
+      responseType = 'WEBSOCKET_MESSAGE_QUESTION_FETCH';
+    }if(action === 'create'){
+      responseType = 'WEBSOCKET_MESSAGE_QUESTION_CREATE';
+    }else if (action === 'reply'){
+      responseType = 'WEBSOCKET_MESSAGE_QUESTION_REPLY';
+    }else if (action === 'markAsAnswered'){
+      responseType = 'WEBSOCKET_MESSAGE_QUESTION_MARK_AS_ANSWERED';
+    }else if (action === 'search'){
+      responseType = 'WEBSOCKET_MESSAGE_QUESTION_SEARCH';
+    }else if (action === 'updatePriority'){
+      responseType = 'WEBSOCKET_MESSAGE_QUESTION_UPDATE_PRIORITY';
+    }else if (action === 'lockUnlock'){
+      responseType = 'WEBSOCKET_MESSAGE_QUESTION_LOCK_UNLOCK';
+    }
+  }else if (command === 'folder') {
+    if(action === 'fetch'){
+      responseType = 'WEBSOCKET_MESSAGE_FOLDER_FETCH';
+    }else if (action === 'add'){
+      responseType = 'WEBSOCKET_MESSAGE_FOLDER_ADD';
+    }else if (action === 'edit'){
+      responseType = 'WEBSOCKET_MESSAGE_FOLDER_EDIT';
+    }else if (action === 'delete'){
+      responseType = 'WEBSOCKET_MESSAGE_FOLDER_DELETE';
+    }else if (action === 'assignQuestion'){
+      responseType = 'WEBSOCKET_MESSAGE_FOLDER_ASSIGNQUESTION';
+    }
+  } 
+  return responseType;
+}
 
 export default {};
